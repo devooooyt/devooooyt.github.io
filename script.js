4c2e1774f5d65d161f1fcf2522fb9ed91a9ae6be
devooooyt <37259482+devooooyt@users.noreply.github.com>
2024-11-20 17:40:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById('overlay').addEventListener('click', function() {
 
     Array.from(document.getElementsByClassName("text")).forEach(element => {
         typeWriterEffect(element, "now playing");
-        setTimeout(() => typeWriterEffect(element, "brent faiyaz - wya"), 2500);
+        setTimeout(() => typeWriterEffect(element, "bryson tiller - don't"), 2500);
         setTimeout(() => typeWriterEffect(element, "@devoooo"), 8000);
     });
 });
